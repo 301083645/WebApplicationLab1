3c3028bbca5d7b6d4a1d0f61a6a5d726675038fb
301083645 <eblee150@gmail.com>
2020-09-18 16:43:04 -0400

    Lab1

---
+++ Assignment2.js
@@ -0,0 +1,43 @@
+﻿
+
+function table()
+{
+   var table = '';
+   var rows = 15;
+   var cols = 15;
+   for(var r = 1; r <= rows; r++){
+   
+   table += '<tr>';
+   for(var c = 1; c <= cols; c++){
+   
+   table += '<td>' + r + ',' +c + '</td>';
+   }
+   
+   table += '</tr>';
+   }
+   
+ document.write('<table border=1 >'  + table + '</table>');
+    
+ }
+ 
+ function table2()
+{
+   var table = '';
+   var rows = document.getElementById("row").value;
+   var cols = document.getElementById("col").value;
+   for(var r = 1; r <= rows; r++){
+   
+   table += '<tr>';
+   for(var c = 1; c <= cols; c++){
+   
+   table += '<td>' + r + ',' +c + '</td>';
+   }
+   
+   table += '</tr>';
+   }
+   
+ document.write('<table border=1 >'  + table + '</table>');
+    
+ }
+
+
